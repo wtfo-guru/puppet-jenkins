06d9c8250c241f094363b78dfe7676880d833ffd
que <qs5779@mail.com>
2019-02-10 22:30:10 -0500

    nc

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,6 +2,7 @@ class jenkins::install {
 
   class { 'java':
     distribution => 'jre',
+    version => '8u201',
   }
 
   case $facts['os']['family'] {
@@ -9,10 +10,11 @@ class jenkins::install {
       apt::source { 'jenkins-stable':
         comment  => 'debian stable repo',
         location => 'https://pkg.jenkins.io/debian-stable',
-        release  => 'stable',
-        repos    => 'binary/',
+        release  => 'binary/',
+        repos    => '',
         key      => {
-          'source'     => 'http://pkg.jenkins.io/debian-stable/jenkins.io.key',
+          'id'     => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
+          'source' => 'http://pkg.jenkins.io/debian-stable/jenkins.io.key',
         },
         include  => {
           'src' => false,
@@ -29,14 +31,6 @@ class jenkins::install {
         gpgcheck => '1',
       }
     }
-  }
-
-  yumrepo { 'jenkins':
-    ensure   => 'present',
-    baseurl  => 'http://pkg.jenkins.io/redhat',
-    descr    => 'Jenkins',
-    gpgkey   => 'https://pkg.jenkins.io/redhat/jenkins.io.key',
-    gpgcheck => '1',
   }
 
   package { 'jenkins':
